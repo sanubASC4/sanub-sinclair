6f72c09fca82b08d846062af1f4b98c3e2f0d352
sanubASC4 <sanubsinclair3@gmail.com>
2017-07-19 13:04:12 -0400

    battleship

---
+++ sanubsinclair-battleship/battleship.js
@@ -0,0 +1,39 @@
+//var cols = 8;
+//var rows = 8;
+//var table;
+
+function setup() {
+    createCanvas(800, 800);
+    background("skyblue")
+   // var totalRows = table.getRowCount();
+
+}
+function draw(){
+    line(100,0,100,800);
+    line(200,0,200,800);
+    line(300,0,300,800);
+    line(400,0,400,800);
+    line(500,0,500,800);
+    line(600,0,600,800);
+    line(700,0,700,800);
+    line(800,0,799,800);
+    line(0,100,800,100);
+    line(0,200,800,200);
+    line(0,300,800,300);
+    line(0,400,800,400);
+    line(0,500,800,500);
+    line(0,600,800,600);
+    line(0,700,800,700);
+    line(0,799,800,799);
+    stroke(0);
+    fill("black");
+    ellipse(40,50,21,21);
+    ellipse(750,650,21,21);
+    ellipse(450,450,21,21);
+    ellipse(250,550,21,21);
+    ellipse(750,650,21,21);
+    ellipse(750,650,21,21);
+    ellipse(750,650,21,21);
+    ellipse(750,650,21,21);
+    ellipse(750,650,21,21);
+}
