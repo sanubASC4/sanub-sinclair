3131577c605faa8df971ad7b52f88419f84d9e32
sanubASC4 <sanubsinclair3@gmail.com>
2017-08-02 11:44:06 -0400

    update

---
+++ sanubsinclair-nestedapis/sketch.js
@@ -0,0 +1,27 @@
+$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+    //   console.log(data.results);
+      var user= data.results[0];
+      var suffix= user.name.title;
+      var firstname = user.name.first;
+      var lastname = user.name.last;
+      var picture = user.picture.large;
+      var nat = user.nat;
+      console.log();
+      $("body").append("<h1>Hello I am " +suffix +" "+firstname+" "+lastname+"  "+picture+" "+nat+"</h1>");
+       funciton (){
+        
+        $.sketch({
+            url: 'https://restcountries.eu/rest/v2/name/{name}?fullText=true',
+            dataType: 'json'
+
+        });
+    }
+    }
+});
+   
+
+
+      
