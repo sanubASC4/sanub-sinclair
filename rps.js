f28d1db80615bb04261ea3c7397655721f8ccc84
sanubASC4 <sanubsinclair3@gmail.com>
2017-08-07 13:52:55 -0400

    update

--- rps.js
+++ rps.js
@@ -1,30 +1,93 @@
-var playerChoice = "r";
-var cpuChoice = "s";
+var loses = 0;
+var wins = 0;
+            
+var play = function(userChoice) {
+                    
+                    document.getElementById("player").innerHTML="";
+                    document.getElementById("opponent").innerHTML="";
+                    document.getElementById("results").innerHTML="";
+                
+                    
+                    if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
+                        document.getElementById("player").innerHTML='You chose' + ' ' + userChoice + '.';
+                    } else if (userChoice == "rope") {
+                        document.getElementById("player").innerHTML='You chose' + ' ' + userChoice + '. <br />Well aren\'t you a smarty pants.';
+                    } else {
+                        document.getElementById("player").innerHTML="That is not a valid choice, try again.";
+                        
+                        return false;
+                    }
+                
+                    var computerChoice = Math.random();
+                    if (computerChoice < 0.34) {
+                        computerChoice = "rock";
+                    } else if(computerChoice <= 0.67) {
+                        computerChoice = "paper";
+                    } else {
+                        computerChoice = "scissors";
+                    }
+                 
+                     document.getElementById("opponent").innerHTML='// Your opponent chose' + ' ' + computerChoice + '.';
+                 
+                     var compare = function (choice1,choice2) {
+                        if (choice1 == choice2) {
+                            return "The result is a tie!";
+                        } else if (choice1 == "rock"){
+                            if (choice2 =="scissors") {
+                                wins++;
+                                return "rock wins. rock on.";
+                            } else {
+                                loses++;
+                                return "sorry. paper wins.";
+                            }
+                        } else if (choice1 == "paper") {
+                            if (choice2 == "rock") {
+                                wins++;
+                                return "paper wins";
+                            } else {
+                                loses++;
+                                return "sorry. scissors win.";
+                            }
+                        } else if (choice1 == "scissors") {
+                            if (choice2 == "rock") {
+                                loses++;
+                                return "sorry. rock wins";
+                            } else {
+                                wins++;
+                                return "scissors win";
+                            }
+                        } else if (choice1 == "rope") {
+                            wins++;
+                            return "rope FTW";
+                        } else {
+                            return "error. bummer dude. game over. no dice.";
+                        }
+                    };
 
-if (playerChoice == "r" && cpuChoice == "p"){
-    console.log("CPU won because it chose paper");
-}
+                    var winner = compare(userChoice,computerChoice);
+                    document.getElementById("results").innerHTML=winner;
+                    document.getElementById("wins").innerHTML=wins;
+                    document.getElementById("loses").innerHTML=loses;
+                    
+                    if (wins > 99 || loses > 99) {
+                        document.getElementById("wins").style.fontSize="44";
+                        document.getElementById("loses").style.fontSize="44";
+                    }
+                    if (wins > 999) {
+                        alert("You reached the max score of 999. <br />Congratulations, you have no life.");
+                    }
+                    if (loses > 999) {
+                        alert("Your opponent reached the max score of 999. <br />We're sorry, you have no life.");
+                    }
+            };
 
-if (playerChoice == "r" && cpuChoice == "s"){
-    console.log("You won because you chose rock");
-}
 
-if (playerChoice == cpuChoice){
-    console.log("It's a draw!");
-}
 
-if (playerChoice == "p" && cpuChoice == "s"){
-    console.log("CPU won because it chose scissors.");
-}
 
-if (playerChoice == "p" && cpuChoice == "r"){
-    console.log("You won b/c you chose paper");
-}
 
-if (playerChoice == "s" && cpuChoice == "p"){
-    console.log("You won b/c you chose scissors");
-}
-
-if (playerChoice == "s" && cpuChoice == "r"){
-    console.log("CPU won b/c it chose rock");
-}
+var reset = function() {
+    loses = 0;
+    wins = 0;
+    document.getElementById("wins").innerHTML=wins;
+    document.getElementById("loses").innerHTML=loses;
+};
