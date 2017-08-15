d09519bb690f8bdc53975dc59225f83d31ff1765
sanubASC4 <sanubsinclair3@gmail.com>
2017-08-15 17:06:02 -0400

    updates

---
+++ finalProject/lib/js/main.js
@@ -0,0 +1,25 @@
+//Set database object
+var database=firebase.database().ref();
+
+//button executes this function
+function updateDB(){
+    var name = $("#name").val();
+    var message = $("#message").val();
+    console.log(name + " : " + message);
+
+    //Update database here
+    var text = {
+        NAME: name,
+        MESSAGE: message
+    }
+    database.push(text);
+}
+
+database.on("child_added", function(rowData){
+    var row = rowData.val();
+    var name = row.NAME;
+    var message= row.MESSAGE;
+
+    var fullText = "<p>"+ name + " : " + message + "</p>";
+    $(".allMessages").append(fullText);
+});
--- finalProject/sketch.js
+++ finalProject/sketch.js
@@ -1,1 +1,40 @@
-$.ajax
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+//called when successful user log in
+function onSignIn(googleUser) {
+    console.log('User signed in!');
+    var profile = googleUser.getBasicProfile();
+    //change userName text, img source, & email text based on profile
+    $(".userName").text(profile.getName());
+    $("img").attr("src", "https://i.ytimg.com/vi/M-EZkvEzqt8/maxresdefault.jpg");
+    $(".email").text(profile.getEmail());
+}
+
+//called when "sign out" button clicked
+function onSignOut() {
+    //should sign user out and toggleHidden
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.')
+        //setting back to default
+        $(".userName").text("USER_NAME");
+        $("img").attr("src", "https://i.ytimg.com/vi/M-EZkvEzqt8/maxresdefault.jpg");
+        $(".email").text("example@example.com");
+    });
+}
