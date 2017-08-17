5e5907d60db3968c574a41f62b3b1e964b5492b7
sanubASC4 <sanubsinclair3@gmail.com>
2017-08-17 10:53:09 -0400

    final

---
+++ finalProject/tst.js
@@ -0,0 +1,10 @@
+$(function(){
+
+$(".sub-btn").click(function(e){
+    e.preventDefault();
+    $("#report").text('Success! You\'ve reported an incident! We\'ll get back to you fairly shortly!'
+);
+
+});
+
+});
